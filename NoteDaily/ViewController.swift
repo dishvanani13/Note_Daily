3eceaaf04ab6f01849bf435a5863275dcfcf1ff8
dishavanani13 <dishvanani13@gmail.com>
2025-01-21 22:23:59 -0500

    Initial Commit

---
+++ NoteDaily/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteDaily
+//
+//  Created by Disha Limbani on 2025-01-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var searchTitle: UISearchBar!
+    
+    @IBOutlet weak var tblViewCell: UITableViewCell!
+    @IBOutlet weak var tblView: UITableView!
+    @IBOutlet weak var btnAddNewNote: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NoteDailyTests/NoteDailyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteDailyTests.swift
+//  NoteDailyTests
+//
+//  Created by Disha Limbani on 2025-01-21.
+//
+
+import Testing
+@testable import NoteDaily
+
+struct NoteDailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
