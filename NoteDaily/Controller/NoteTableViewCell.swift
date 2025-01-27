243940870039e5f1c63fe7bd3185262315de9a45
dishavanani13 <dishvanani13@gmail.com>
2025-01-26 22:14:01 -0500

    set up coredata and tring to Save

---
+++ NoteDaily/Controller/NoteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoteTableViewCell.swift
+//  NoteDaily
+//
+//  Created by Disha Limbani on 2025-01-26.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
