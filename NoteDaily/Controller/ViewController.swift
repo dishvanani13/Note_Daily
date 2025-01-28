c6497e18e5f342e6b65e50678bdb4e02b3ca4173
dishavanani13 <dishvanani13@gmail.com>
2025-01-27 21:42:53 -0500

    fetch data and reload in tableview

--- NoteDaily/Controller/ViewController.swift
+++ NoteDaily/Controller/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchTitle: UISearchBar!
    // Variables
     var noteList = [Note]()
-   
+    var firstLoad = true
     
    // outlets
     @IBOutlet weak var tblView: UITableView!
@@ -24,6 +24,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         tblView.dataSource = self
         tblView.delegate = self
+        if(firstLoad)
+        {
+            firstLoad = false
+            let appdelegate = UIApplication.shared.delegate as! AppDelegate
+            let context: NSManagedObjectContext = appdelegate.persistentContainer.viewContext
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Note")
+            do {
+                let results:NSArray = try context.fetch(request) as NSArray
+                for result in results {
+                    noteList.append(result as! Note)
+                }
+            }catch {
+                print("error \(error)")
+            }
+        }
     }
 
     @IBAction func btnAddNote(_ sender: UIButton) {
@@ -46,6 +61,9 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource{
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        tblView.reloadData()
     }
     
 }
